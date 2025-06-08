cced12675737a2737114a3ab1b445992dd341c52
jencba <jdamian@elam.co.uk>
2025-06-08 17:08:04 +0100

    port changed

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require('./app');
-const { PORT = 5001 } = process.env;
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
